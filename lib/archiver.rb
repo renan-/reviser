e4453936ec15663657a47077b792d59b6eb9cb57
Yann Prono <pronoyann@gmail.com>
2014-12-28 16:18:09 +0100

    Check paramaters of constructor

--- lib/archiver.rb
+++ lib/archiver.rb
@@ -1,14 +1,33 @@
+require 'fileutils'
 
 # Manage uncompression of archive.
+# The archive contains all computing projects
+#
+# Author::	Yann Prono
 
 class Archiver
-
-	def initialize(archive,p_dir)
+	
+	# Get archive to use and the path directory.
+	def initialize(archive,path_dir)
 		@src = archive
-		@destination = p_dir
+		@destination = path_dir
+
+		begin
+			# Raise exception if the archive doesn't exist
+			raise "L'archive '#{@src}' n'existe pas." unless File.exists? @src
+			# Create directory
+			FileUtils.mkdir @destination, :mode => 0700
+			
+			# Exception when the directory already exists
+			rescue Errno::EEXIST => e
+				puts "Le dossier '#{@destination}' existe deja."
+			
+			# Exception when the archive doesn't exist
+			rescue Exception => e
+				puts e.message
+	 	end
 	end
 
 end
-
 
 a = Archiver.new("src","fodler")
