cee45ba5db348224e57843fa06c5bfecdfab58a9
Yann Prono <pronoyann@gmail.com>
2014-12-30 18:24:47 +0100

    Add comments, fix bug of overwriting (folder)

--- lib/zipextractor.rb
+++ lib/zipextractor.rb
@@ -8,15 +8,24 @@ class ZipExtractor < Extractor
 
 	def initialize(archive,path_dir)
 		super(archive, path_dir,'zip')
+		Zip.on_exists_proc = true
 		Zip.continue_on_exists_proc = true
 	end
 
 	def extract 
+		super
+		# Open file
 		Zip::File.open(@src) do |zip_file|
+			#Entry = file or directory
   			zip_file.each do |entry|
-  				path = File.join(@destination, entry.name)
-    			entry.extract(path)
-    			content = entry.get_input_stream.read
+  				#Create filepath
+  				filepath = File.join(@destination, entry.name)
+  				# Check if it doesn't exist because of directories (overwrite)
+  				if(!File.exist?(filepath))
+  					# Create directories to access file
+  					FileUtils.mkdir_p(File.dirname(filepath))
+    				entry.extract(filepath) 
+    			end
   			end
 		end
 	end
