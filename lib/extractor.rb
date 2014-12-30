7379b13464e3437902a4417e9755f914606f7fbd
Yann Prono <pronoyann@gmail.com>
2014-12-30 13:11:06 +0100

    Docomentation

--- lib/extractor.rb
+++ lib/extractor.rb
@@ -1,12 +1,18 @@
+# Abstract archive Extractor.
 class Extractor
 
+	# Initialize the extractor
+	# A Extractor is defined by its type (ZIP, TAR, RAR ...)
 	def initialize(archive,path_dir, type)
 		@src = archive
 		@destination = path_dir
 		@type = type
 	end
 
+	# Abstract method extract
+	# Extract the archive into the directory.
+	# TO-REVISE
 	def extract
 	end
-	
+
 end
