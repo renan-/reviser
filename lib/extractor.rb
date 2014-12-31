7004b33064699f6b7d24dc79b9a83dc48912a511
Yann Prono <pronoyann@gmail.com>
2014-12-31 12:14:46 +0100

    Use modules now

--- lib/extractor.rb
+++ lib/extractor.rb
@@ -1,18 +1,9 @@
+require_relative 'extractors'
+
 # Abstract archive Extractor.
 class Extractor
 
-	# Initialize the extractor
-	# A Extractor is defined by its type (ZIP, TAR, RAR ...)
-	def initialize(archive,path_dir, type)
-		@src = archive
-		@destination = path_dir
-		@type = type
-	end
+	extend Extractors
 
-	# Abstract method extract
-	# Extract the archive into the directory.
-	# TO-REVISE
-	def extract
-	end
-
+	
 end
