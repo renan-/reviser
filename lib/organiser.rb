0c2634c705d9579162a5fabc312a9910ed782bd0
Yann Prono <pronoyann@gmail.com>
2015-01-03 19:19:57 +0100

    Add comments, rename par default if no match for regex

--- lib/organiser.rb
+++ lib/organiser.rb
@@ -5,17 +5,31 @@ require 'fileutils'
 # Author::	Yann Prono
 class Organiser
 
+	# initialize tool
 	def initialize(directory_working)
 		@directory = directory_working
 	end
 
+	# Method which renames project's folder.
 	def run
+		# If the regex doesn"t match
+		count = 0;
+		
+		# get all entries of projects folder
 		entries = Dir.entries(@directory).reject{|entry| entry == "." || entry == ".."}
-		entries.each do |entry|
+		entries.each do |entry|			
+
+			#apply regex and take first match
 			name = entry.scan(/^[a-zA-Z]+[ ]+[a-zA-Z]+/).first
-			name = File.join(@directory, name)
 			entry = File.join(@directory, entry)
-			FileUtils.mv(entry, name) 
+			if(name != nil)
+				name = File.join(@directory, name)
+			else
+				#Name folder is number if no match
+				name = File.join(@directory, "#{count}")
+				count+= 1
+  			end
+  			FileUtils.mv(entry, name)
   		end
 	end
 
