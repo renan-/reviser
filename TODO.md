18860d7cf2e2b2ad12ef5b5f4986140c2b1f2dc6
Yann Prono <pronoyann@gmail.com>
2015-01-08 23:32:03 +0100

    Add new tests

--- TODO.md
+++ TODO.md
@@ -5,8 +5,15 @@
 
 * Organiser renomme les fichiers avec un regex en brute dans le code
 * Les archives multiformats (tar.gz, zip.gz ? ...)
+* Si le dossier des projects existe déja, il est supprimé en -rf (sans autorisation)
 
 #Choses à faire
 
 * Tests unitaires
+* Tester exceptions
 * Diagrammes de classes
+
+
+#Idées
+
+* utiliser un loggeur ?
--- test/test_archiver.rb
+++ test/test_archiver.rb
@@ -2,6 +2,7 @@ require_relative "../lib/archiver"
 require_relative "../lib/component"
 require_relative "../lib/reviser"
 require "test/unit"
+require "fileutils"
 
 #
 # Test the Archiver class
@@ -13,16 +14,40 @@ class TestArchiver < Test::Unit::TestCase
 	def setup
 		Component::setup 'config.yml'
 		@archiver = Archiver.new(nil)
+		@file = @archiver.src
+		@dest = @archiver.destination
 		@nb_projects = 15
 	end
 
 	def teardown 
+		FileUtils.rm_rf(@dest)
 	end
 
+	# Verify constructor
+	def test_initialize
+		assert(Dir.exists?(@dest), "The directory #{@dest} should exist")
+	end
+
+	# Normal case
+	def test_extract
+		Archiver.extract(@file, @dest)
+		entries = Dir.entries(@dest).reject{|entry| entry == '.' || entry == '..'}
+		assert(entries.size >= 1, "the extraction should extract some entries")
+	end
+
+	# File not found
+	# TODO find how to test exception
+	def test_extract_no_file
+		raise = false
+		Archiver.extract("coucou.zip", @dest)
+		assert_raise Errno::ENOENT do
+		end
+	end
+
+	
 	def test_run
 		@archiver.run
-		entries = Dir.entries(@archiver.destination).reject{|entry| entry == '.' || entry == '..'}
-		assert(Dir.exists?(@archiver.destination), "The directory #{@archiver.destination} should exist")
+		entries = Dir.entries(@dest).reject{|entry| entry == '.' || entry == '..'}
 		assert_equal(@nb_projects, entries.size, "the directory was expected to contain #{@nb_projects} directories.")
 	end
 
