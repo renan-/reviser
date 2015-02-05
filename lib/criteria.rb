4a570621e1963d7f6ddf76348e1767856b759d95
Yann Prono <pronoyann@gmail.com>
2015-02-05 14:34:18 +0100

    Now, all criterias are stored in a hash (criterias => module)

--- lib/criteria.rb
+++ lib/criteria.rb
@@ -1,9 +1,12 @@
 
-# Represents a criterias
+# Manages criterias.
+#
 # @author Yann Prono
 class Criteria
 
-	@@loaded = {}
+	# All criterias available.
+	@@criterias = {}
+
 	# Load all of modules available for the analysis
 	def self.load
 		modules =  Dir['*tool*']
@@ -11,22 +14,25 @@ class Criteria
 			require_relative m
 			name_module = camelize m
 			methods = Module.const_get(name_module).instance_methods
-			Criteria.populate(name_module, methods)
+			methods.each { |method| populate(method.to_sym, name_module) }
  		end
-
- 		puts @@loaded.inspect
  	end
 
-	# Get name of module
+	# Gets the name of module 
+	# @param file_module Name of the file module.
 	def self.camelize(file_module) 
 		File.basename(file_module).delete('.rb').split('_').each {|s| s.capitalize! }.join('')
 	end
 
-	def self.populate(k,v) 
-		@@loaded[k.to_sym] = v
+private 
+
+	#  from Cfg file to symbols
+	# @param criteria The criteria
+	# @param module_name The name of the module.
+	def self.populate(criteria, module_name) 
+		@@criterias[criteria.to_sym] = module_name
 	end
 
 end
-
 
 Criteria.load
