973ec644aaac48f2ff194e4c058ff1e734f46b2d
Yann Prono <pronoyann@gmail.com>
2015-02-06 09:45:36 +0100

    Add init method for command line which copying config file

--- lib/app/exec.rb
+++ lib/app/exec.rb
@@ -1,20 +1,40 @@
 require 'fileutils'
 
+# Module used for managing all actions in command line
 module Exec
 
+	# path of config template file.
+	$template_path = File.join(File.dirname(File.dirname(File.dirname(__FILE__))),'config.yml')
+
+	# Say hello to the user !
 	def hello
 		puts 'Hello, this is my app'
 	end
 
+	# Get the action of user and execute the good action
+	# TODO
 	def exec(*argv)
-		self.send(argv[0].first)
+		unless argv[0].empty?
+			self.send(argv[0].first)
+		end
 	end
 
-
+	# Create a environnment for checking projects
+	# This method only copies the config file into the current directory.
 	def init
-		puts FileUtils.pwd
-		puts 'Welcome in init'
-
-		File.open('jfdkfkd','w')
+		pwd = FileUtils.pwd		
+		FileUtils.cp($template_path,pwd)
+		print "\n\tCreate\t#{File.basename $template_path}\n\n"
 	end
+
+
+	# In case of the action is unknown
+	def method_missing(m, *args, &block)  
+		puts "Usage:\n\tapp <action> <paramaters>*"
+		puts "\nParamaters could be optionnal."
+		puts "\nActions :"
+		ls = Exec.instance_methods(true) - [:method_missing, :exec]
+		ls.each { |action| puts "\t#{action}" }
+  	end
+
 end
