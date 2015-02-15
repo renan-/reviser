87a648f44097c3ccc24369d73989d16f8e73a45a
Yann Prono <pronoyann@gmail.com>
2015-02-15 18:47:03 +0100

    Optimize require for extractors (cc Rerer)

--- lib/extractors.rb
+++ lib/extractors.rb
@@ -1,9 +1,5 @@
 require 'rubygems'
-require 'rubygems/package'
-require 'zip'
-require 'zlib'
 require 'fileutils'
-require 'shellwords'
 
 # The module contains all methods to uncompress a archive 
 # regardless the format.
@@ -11,14 +7,15 @@ require 'shellwords'
 # To add a new format, maybe you need to install the Gem.
 # Find a Gem which uncompress a specified format on rubygems.org.
 # Add the line "gem <gem>" in the Gemfile and execute "bundle install"
-# Next, you have to require the gem: "require <gem>" in the header of this file.
 #
-# Now, you can right the method corresponding to the format.
+# Now, you can write the method corresponding to the format.
 # The name of the method corresponds to the format.
 # For example, if you to use rar format, the name of the method will be: "rar"
+# Don't forget to require the gem: "require <gem>" at the begininng of the method !
 # the header of method looks like the following block:
 #
 #  		def <format> (src, destination)
+# 			require <gem>
 # 			...
 # 		end
 #
@@ -30,6 +27,7 @@ module Extractors
 	# ZIP format
 	#
 	def zip (src, destination)
+		require 'zip'
 		# Cfg the gem
 		Zip.on_exists_proc = true
 		Zip.continue_on_exists_proc = true
@@ -54,6 +52,7 @@ module Extractors
 	# gzip format
 	#
 	def gz (tarfile,destination)
+		require 'zlib'
       	z = Zlib::GzipReader.open(tarfile)
       	unzipped = StringIO.new(z.read)
       	z.close
@@ -69,6 +68,7 @@ module Extractors
 	# tar format
 	#
 	def tar (src,destination)
+		require 'rubygems/package'
     	# test if src is String (filename) or IO stream
     	if src.is_a? String
     		stream = File.open(src)
@@ -97,6 +97,7 @@ module Extractors
  	# if it is possible.
  	#
 	def rar(src,destination)
+		require 'shellwords'
  		`which unrar`
  		if $?.success?
  			src = Shellwords.escape(src)
@@ -112,7 +113,6 @@ module Extractors
 	#
 	def seven_zip(src, destination)
 		require 'seven_zip_ruby'
-		
 		File.open(src, 'rb') do |file|
   			SevenZipRuby::Reader.open(file) do |szr|
     			szr.extract_all destination
