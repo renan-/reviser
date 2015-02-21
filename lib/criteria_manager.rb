99711851902375404e0e466f9d33042a371bb608
Yann Prono <pronoyann@gmail.com>
2015-02-21 18:52:10 +0100

    transform criteria manager into an full object (all static methods before)

--- lib/criteria_manager.rb
+++ lib/criteria_manager.rb
@@ -1,4 +1,4 @@
-
+require_relative 'config'
 # Manage criteria.
 # Criteria provides all criteria available 
 # that can be used for the analysis.
@@ -10,54 +10,61 @@
 #
 # @author Yann Prono
 #
-class Criteria
+class CriteriaManager
 
 	# All criterias available.
 	# :criterion => Name of the module
-	@@criterias = {}
+	@criteria
+
+	def initialize
+		@criteria = Hash.new
+		load
+		prepare
+	end
 
 	# Load all of modules available for the analysis
-	def self.load		
+	def load		
 		modules =  Dir[File.join(File.dirname(__FILE__),'*tool*')]
-				
+		
 		modules.each do |m|
 			require_relative m
-			name_module = camelize m
-			methods = Module.const_get(name_module).instance_methods
-			methods.each { |method| populate(method.to_sym, name_module) }
- 		end
+			module_name = camelize m
+			methods = Module.const_get(module_name).instance_methods
+			methods.each { |method| populate(method, module_name) }
+ 		end	
  	end
 
 	# Gets the name of module 
 	# @param file_module Name of the file module.
-	def self.camelize(file_module) 
+	def camelize(file_module) 
 		File.basename(file_module).delete('.rb').split('_').each {|s| s.capitalize! }.join('')
 	end
 
 	# Gets all criterias which can be used.
 	# @return [Array] all criterias
-	def self.all
-		@@criterias.keys.map &:to_sym
+	def all
+		@criteria.keys.map &:to_sym
 	end
 
 	# Prepare all criterias provided by the user in the config file.
 	# @param [Array] config Contains all criterias the user wants
-	def self.prepare(config)
+	def prepare
+		puts Cfg[:criteria]
 		# Get all criterias to delete
-		to_delete = config.empty? ? {} : all - (config.map &:to_sym)
+		#to_delete = Cfg[:criteria].empty? ? {} : all - (config.map &:to_sym)
 
 		# Delete now !
-		to_delete.each {|criteria| @@criterias.delete(criteria.to_sym)}
+		#to_delete.each {|criteria| @criterias.delete(criteria.to_sym)}
 	end
 
 
-private 
+	private 
 
 	#  from Cfg file to symbols
-	# @param criteria The criteria
+	# @param criterion The criteria
 	# @param module_name The name of the module.
-	def self.populate(criteria, module_name) 
-		@@criterias[criteria.to_sym] = module_name
+	def populate(criterion, module_name)
+		@criteria[criterion.to_sym] = module_name
 	end
 
 end
