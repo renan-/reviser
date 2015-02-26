0c3dfea9ce041e6c2e0ee4b44f410bc1209fd494
Renan Strauss <renan.strauss@gmail.com>
2015-02-26 11:31:43 +0100

    Basic criterias handling directly in Checker, what do you think Yann ? :-)

--- lib/code_analysis_tools.rb
+++ lib/code_analysis_tools.rb
@@ -6,11 +6,8 @@
 
 module CodeAnalysisTools
 
-	#
-	# @return all the files in the project's folder
-	#
-	def files
-		Dir.glob("**/*").select { |f| (File.file?(f)) }
+	def all_files
+		files.join("\r")
 	end
 
 	#
@@ -18,14 +15,14 @@ module CodeAnalysisTools
 	# 		  extenstion language list (note that Cfg[:extension] must be an array)
 	#
 	def src_files
-		files.select { |f| Cfg[:extension].include? File.extname(f) }
+		sources.join("\r")
 	end
 
 	#
 	# @return the total amount of lines of code
 	#
 	def lines_count
-		count = src_files.inject(0) { |sum, f|
+		count = sources.inject(0) { |sum, f|
 			sum + File.open(f).readlines.select { |l| !l.chomp.empty? }.size
 		}
 
@@ -36,7 +33,7 @@ module CodeAnalysisTools
 	# @return the number of lines of comments
 	#
 	def comments_count
-		tab_comments = src_files.inject([]) { |t, f| t << IO.read(f).scrub.scan(Cfg[:regex_comments]) }
+		tab_comments = sources.inject([]) { |t, f| t << IO.read(f).scrub.scan(Cfg[:regex_comments]) }
 		lines = tab_comments.inject('') { |s, comm| s << find_comments(comm) }.split "\n"
 
 		lines.size
@@ -44,6 +41,21 @@ module CodeAnalysisTools
 
 private
 
+	#
+	# @return all the files in the project's folder
+	#
+	def files
+		Dir.glob("**/*").select { |f| (File.file?(f)) }
+	end
+
+	def sources
+		files.select { |f| Cfg[:extension].include? File.extname(f) }
+	end
+
+	#
+	# Translates a sub-match returned by scan
+	# into raw comments string
+	#
 	def find_comments(comm)
 		comm.inject('') { |t, l| t << l.detect { |a| (a != nil) && !a.strip.empty? } + "\n" }
 	end
