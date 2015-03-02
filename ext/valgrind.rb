a8681465467be3d702f791b71078a1743c7d3bf5
Yann Prono <pronoyann@gmail.com>
2015-03-02 23:06:34 +0100

    Implements valgrind method for memory leaks (Maybe change representation of results for a file for each project)

--- ext/valgrind.rb
+++ ext/valgrind.rb
@@ -1,5 +1,22 @@
+require_relative '../lib/utils'
+require_relative '../lib/execution_tools'
+
 module Valgrind
+
+	include ExecutionTools
+
+	include Utils
+
+	#
+	# Check memory leaks of the program
+	# The module uses execution value written in the config file of the project 
+	#
 	def memleaks
-		'Dat memleak'
+		executable = find_executable
+		program = "#{Cfg[:program_prefix]}#{executable}"
+		param = Cfg.has_key?(:execution_value) ? Cfg[:execution_value].first : ''
+		cmd = "valgrind --leak-check=full --track-origins=yes --show-reachable=yes #{program} #{param}"
+		out = exec_with_timeout cmd
+		"$ #{cmd}\r#{out[:stdout]}\r#{out[:stderr]}"
 	end
 end
