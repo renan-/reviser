a8e86b5620df2210da8665f46a08b1c1c577d902
Renan Strauss <renan.strauss@gmail.com>
2015-03-31 11:59:10 +0200

    Update example

--- example/my_generator.rb
+++ example/my_generator.rb
@@ -23,10 +23,16 @@ class MyGenerator < Reviser::Component
 	def run
 		out = '<!DOCTYPE html><html><head>'
 		out += '<meta charset= "UTF-8">'
-		out += "<link rel=\"stylesheet\" type=\"text/css\" href=\"#{resource('css/jquery.dataTables.css').to_path}\">"
-		out += "<script type=\"text/javascript\" src=\"#{resource('js/jquery-1.11.1.min.js').to_path}\"></script>"
-		out += "<script type=\"text/javascript\" src=\"#{resource('js/jquery.dataTables.min.js').to_path}\"></script>"
+		out += "<link rel=\"stylesheet\" type=\"text/css\" href=\"http://netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css\">"
+		out += "<link rel=\"stylesheet\" type=\"text/css\" href=\"http://cdn.datatables.net/plug-ins/f2c75b7247b/integration/bootstrap/3/dataTables.bootstrap.css\">"
+		out += "<link rel=\"stylesheet\" type=\"text/css\" href=\"#{resource('css/main.css').to_path}\">"
+		out += "<script type=\"text/javascript\" src=\"http://code.jquery.com/jquery-1.10.2.min.js\"></script>"
+		out += "<script type=\"text/javascript\" src=\"http://cdn.datatables.net/1.10.5/js/jquery.dataTables.min.js\"></script>"
+		out += "<script type=\"text/javascript\" src=\"http://cdn.datatables.net/plug-ins/f2c75b7247b/integration/bootstrap/3/dataTables.bootstrap.js\"></script>"
 		out += '<title>Results</title>'
+		out += "<script type=\"text/javascript\">"
+		out += "$(document).ready(function() { $('#results').dataTable({\"dom\":' <\"search\"fl><\"top\">rt<\"bottom\"ip><\"clear\">'}); });"
+		out += "</script>"
 		out += "</head>\n<body><table id=\"results\"><thead><tr>"
 
 		@data.values.first.keys.unshift.unshift('Projet').each { |crit| out += "<th>#{crit}</th>" }
@@ -36,14 +42,14 @@ class MyGenerator < Reviser::Component
 		@data.keys.each do |proj|
 			out += "<tr><th>#{proj}</th>"
 			@data[proj].each do |k, v|
-				out += "<td><pre>#{v.to_s.strip.scrub}</pre></td>"
+				out += "<td>#{v.to_s.strip.scrub}</td>"
 			end
 			out += '</tr>'
 		end
 
 		out += '</tbody></table>'
-		out += "<script type=\"text/javascript\">"
-		out += "$(document).ready(function() { $('#results').DataTable(); });"
+		out += "<script type=\"text/javascript\">"	
+		out += "$('#results').removeClass( 'display' ).addClass('table table-striped table-bordered');"
 		out += "</script>"
 		out += '</body></html>'
 
