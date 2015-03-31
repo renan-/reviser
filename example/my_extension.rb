98fd86ec6b3336ad9b226d57fc106325b68c9a6c
Renan Strauss <renan.strauss@gmail.com>
2015-03-31 16:44:41 +0200

    Modify sample extension so that it does something useful :-)

--- example/my_extension.rb
+++ example/my_extension.rb
@@ -19,6 +19,8 @@
 #   You should have received a copy of the GNU General Public License
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
+require 'find'
+
 require_relative '../lib/reviser'
 
 #
@@ -29,15 +31,30 @@ module MyExtension
 	# This helper has the 'sources' methods
 	# that allow you to retrieve all sources
 	# files (files matching language extension)
+	# as well as the 'manufacture' method,
+	# needed to adapt results to out format
 	#
 	include Reviser::Helpers::Project
 
-	def my_criteria
-		results = []
-		sources.each do |f|
-			results << f
+	#
+	# Keep in mind that this method will be run
+	# in each project's directory !
+	# (that's why we use FileUtils.pwd)
+	#
+	def project_size
+		#
+		# That's the way we require gems without worrying
+		# of them being installed or not !
+		# require_gem is in global scope, and will raise an
+		# exception if the given gem is not installed.
+		# It will include it otherwise
+		#
+		unless defined? Filesize
+			require_gem 'filesize'
 		end
 
+		size_in_bytes = size(FileUtils.pwd)
+		results = Filesize.from("#{size_in_bytes} B").pretty
 		#
 		# The manufacture method excepts
 		# a block which must describe the result contents
@@ -46,23 +63,22 @@ module MyExtension
 		# for all formats.
 		#
 		manufacture do |format|
-			format.html { to_html results }
-			format.csv { ['This', 'is', 'power', 'of', 'Ruby', 'blocks'] }
+			format.html { '<span style="font-weight:bold;">' + results + '</span>' }
+			format.csv { results }
 			format.xls { results }
 		end
 	end
 
 	private
 		#
-		# We just create a HTML list
+		# Returns the size of all the given
+		# files (even if there are also dirs)
+		# in bytes
 		#
-		def to_html data
-			html = '<ul>'
-			data.each do |el|
-				html << "<li>#{el}</li>"
-			end
-			html << '</ul>'
+		def size(dir)
+			bytes = 0
+			Find.find(*Dir["#{dir}/**/*"]) { |f| bytes += File.stat(f).size }
 
-			html
+			bytes.to_f
 		end
 end
